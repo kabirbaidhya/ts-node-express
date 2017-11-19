144177669ac5a00f283a84fa43dbcc8b4558e833
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-11-19 12:34:35 +0545

    Rename ecosystem.config.js -> pm2.config.js

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "api",
+      // If boss is used for deployment, set the script path to: current/dist/index.js
+      script: "current/dist/index.js",
+      // For your standalone pm2 based deployment, you can set script to dist/index.js
+      cwd: process.env.PWD,
+      instances: process.env.APP_INSTANCES || 2,
+    },
+  ],
+};
