be2ef61d40fa6327665e70d7019afa48f8baa7b5
Kabir Baidhya <kabirbaidhya@gmail.com>
2018-12-01 13:22:46 +0545

    Node deployment using boss

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "api",
       // If boss is used for deployment, set the script path to: current/dist/index.js
-      script: "current/dist/index.js",
+      script: "dist/index.js",
       // For your standalone pm2 based deployment, you can set script to dist/index.js
       cwd: process.env.PWD,
       instances: process.env.APP_INSTANCES || 2,
